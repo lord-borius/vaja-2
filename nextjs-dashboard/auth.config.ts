aefc29c0bfb0800a5dd530e6e985213bb9c29a9b
lord-borius <aleks.pucnik@gmail.com>
2025-10-14 10:32:22 +0200

    ch 15

--- nextjs-dashboard/auth.config.ts
+++ nextjs-dashboard/auth.config.ts
@@ -1,21 +1,41 @@
-import type { NextAuthConfig } from 'next-auth';
- 
+import Credentials from "next-auth/providers/credentials"
+import { z } from "zod"
+import bcrypt from "bcrypt"
+import postgres from "postgres"
+import type { User } from "@/app/lib/definitions"
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" })
+
+async function getUser(email: string): Promise<User | undefined> {
+  const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`
+  return user[0]
+}
+
 export const authConfig = {
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsed = z
+          .object({
+            email: z.string().email(),
+            password: z.string().min(6),
+          })
+          .safeParse(credentials)
+
+        if (!parsed.success) return null
+        const { email, password } = parsed.data
+        const user = await getUser(email)
+        if (!user) return null
+
+        const passwordsMatch = await bcrypt.compare(password, user.password)
+        if (!passwordsMatch) return null
+
+        return user
+      },
+    }),
+  ],
   pages: {
-    signIn: '/login',
+    signIn: "/login", // optional
   },
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      }
-      return true;
-    },
-  },
-  providers: [], // Add providers with an empty array for now
-} satisfies NextAuthConfig;
+  session: { strategy: "jwt" },
+}
